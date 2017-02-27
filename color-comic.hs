6f10739c72371d9cada4dcb8a5ab17d00581bfb5
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-27 17:54:40 -0600

    Update color-comic.hs

--- color-comic.hs
+++ color-comic.hs
@@ -79,10 +79,88 @@ frameOne = do
     setSGR [SetColor Background Vivid Red]
     putStrLn "&&&&&&&&&&&&&&&&"
     stop
-   
     
+frameTwo :: IO ()
+frameTwo = do
+--one
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+    
+--two
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+   
+--three
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStr "&&&"
+   
+    setSGR [SetColor Foreground Vivid Cyan]
+    setSGR [SetColor Background Vivid Cyan]
+    putStr "&&"
+    
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStr "&&&&&&"
+    
+    setSGR [SetColor Foreground Vivid Cyan]
+    setSGR [SetColor Background Vivid Cyan]
+    putStr "&&"
+    
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&"
+    stop
+    
+--four
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+    
+    
+--five
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+
+--six
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStr "&&&&"
+    setSGR [SetColor Foreground Vivid Cyan]
+    setSGR [SetColor Background Vivid Cyan]
+    putStr "&&&&&&&&"
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&"
+    stop
+  
+    
+--seven
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+   
+  
+--eight
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+   
+   
+  
 main = do
     frameOne
+    stop
+    frameTwo
     stop
 
         
