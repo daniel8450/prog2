b37b39bfb148723567e2a36684ebb24c5a6108fe
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-27 16:40:54 -0600

    Update color-comic.hs

--- color-comic.hs
+++ color-comic.hs
@@ -1,1 +1,9 @@
 import System.Console.ANSI
+
+sgrExample :: IO ()
+sgrExample = do
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Blue]
+    putStr "Red-On-Blue"
+    setSGR [Reset]
+    putStr "White-On-Black"
