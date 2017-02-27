556ee12b9cd8aa154347c6536bd6ef29b6d15882
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-27 17:23:07 -0600

    Update color-comic.hs

--- color-comic.hs
+++ color-comic.hs
@@ -1,27 +1,76 @@
 import System.Console.ANSI
 
-colorOne :: IO ()
-colorOne = do
+rowOne :: IO ()
+rowOne = do
     setSGR [SetColor Foreground Vivid Red]
     setSGR [SetColor Background Vivid Red]
     putStrLn "&&&&&&&&&&&&&&&&"
+    setSGR[Reset]
     clearFromCursorToScreenEnd
 
-colorTwo :: IO ()
-colorTwo = do
+rowTwo :: IO ()
+rowTwo = do
     setSGR [SetColor Foreground Vivid Blue]
     setSGR [SetColor Background Vivid Blue]
     putStrLn "&&&&&&&&&&&&&&&&"
-    --putStrLn "[][][][][][][][][][][][][][][][]"
-  --  putStrLn "[][][][][][][][][][][][][][][][]"
- --   putStrLn "[][][][][][][][][][][][][][][][]"
---    putStrLn "[][][][][][][][][][][][][][][][]"
- --   putStrLn "[][][][][][][][][][][][][][][][]"
-  --  putStrLn "[][][][][][][][][][][][][][][][]"
-   -- setSGR[Reset]
+    setSGR[Reset]
+    clearFromCursorToScreenEnd
+    
+rowThree :: IO ()
+rowThree = do
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    setSGR[Reset]
+    clearFromCursorToScreenEnd
+  
+rowFour :: IO ()
+rowFour = do
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    setSGR[Reset]
+    clearFromCursorToScreenEnd
+    
+rowFive :: IO ()
+rowFive = do
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    setSGR[Reset]
+    clearFromCursorToScreenEnd
+
+rowSix :: IO ()
+rowSix = do
+    setSGR [SetColor Foreground Vivid Blue]
+    setSGR [SetColor Background Vivid Blue]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    setSGR[Reset]
+    clearFromCursorToScreenEnd
+    
+rowSeven :: IO ()
+rowSeven = do
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    setSGR[Reset]
+    clearFromCursorToScreenEnd
+  
+rowEight :: IO ()
+rowEight = do
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    setSGR[Reset]
     clearFromCursorToScreenEnd
     
 main = do
-    colorOne 
-    colorTwo
+    rowOne 
+    rowTwo
+    rowThree
+    rowFour
+    rowFive
+    rowSix
+    rowSeven
+    rowEight
         
