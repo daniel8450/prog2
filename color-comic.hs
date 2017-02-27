aba3f64ae772c25303c4392e9b5daf2c7dc2a502
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-27 16:55:57 -0600

    Update color-comic.hs

--- color-comic.hs
+++ color-comic.hs
@@ -4,7 +4,7 @@ color :: IO ()
 color = do
     setSGR [SetColor Foreground Vivid Red]
     setSGR [SetColor Background Vivid Blue]
-    setSGR [SetBlinkSpeed RapidBlink, "Rapid Blink"]
+    setSGR [SetBlinkSpeed RapidBlink]
     putStr "Red-On-Blue"
     setSGR [Reset]
     putStr "White-On-Black"
