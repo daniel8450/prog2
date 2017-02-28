6a69cf6feb28d1ff2fcaff83b48f082524ad7af8
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-27 19:17:29 -0600

    Update asciimation.hs

--- asciimation.hs
+++ asciimation.hs
@@ -1,1 +1,103 @@
 
+import System.Console.ANSI
+import Control.Concurrent
+
+stop :: IO ()
+stop = do
+    setSGR[Reset]
+    clearFromCursorToScreenEnd
+    
+pause :: IO ()
+pause = do
+    threadDelay 1000000
+    
+frameOne :: IO ()
+frameOne = do
+--one
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+    
+--two
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+   
+--three
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStr "&&&"
+   
+    setSGR [SetColor Foreground Vivid Cyan]
+    setSGR [SetColor Background Vivid Cyan]
+    putStr "&&"
+    
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStr "&&&&&&"
+    
+    setSGR [SetColor Foreground Vivid Cyan]
+    setSGR [SetColor Background Vivid Cyan]
+    putStr "&&"
+    
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&"
+    stop
+    
+--four
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+
+    
+--five
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+
+--six
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStr "&&&&"
+    setSGR [SetColor Foreground Vivid Cyan]
+    setSGR [SetColor Background Vivid Cyan]
+    putStr "&&&&&&&&"
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&"
+    stop
+  
+    
+--seven
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+   
+  
+--eight
+    setSGR [SetColor Foreground Vivid Red]
+    setSGR [SetColor Background Vivid Red]
+    putStrLn "&&&&&&&&&&&&&&&&"
+    stop
+    
+loop :: IO ()
+loop = do
+    clearFromCursorToScreenBeginning
+    setCursorPosition 0 0
+    frameOne
+    pause
+    clearFromCursorToScreenBeginning
+    setCursorPosition 0 0
+    frameTwo
+    pause
+    clearFromCursorToScreenBeginning
+    setCursorPosition 0 0
+    loop
+  
+main = loop
