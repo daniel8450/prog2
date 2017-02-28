b69d0b875d8c53a63131c8ff0e6ba3a7873a2046
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-28 00:23:12 -0600

    Update asciimation.hs

--- asciimation.hs
+++ asciimation.hs
@@ -1339,11 +1339,11 @@ frameEleven = do
     stop
 --six
     blue
-    putStr "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    putStr "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
     cyan
     putStr "....."
     blue
-    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&"
+    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&&"
     stop
 --seven
     blue
@@ -1449,6 +1449,138 @@ frameEleven = do
     blueLine
     blueLine
     blueLine
+    
+-- ==================================================================================
+frameTwelve :: IO ()
+frameTwelve = do
+ --one
+    blueLine
+  --two  
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    cyan
+    putStr "..."
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    stop
+--three
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    cyan
+    putStr "....."
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    stop
+--four
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    cyan
+    putStr "..."
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    stop
+--five
+    blueLine
+--six
+    blueLine
+--seven
+    blueLine
+--eight
+    blueLine
+--nine
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    red
+    putStr "II"
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    stop
+--ten
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&&"
+    red
+    putStr "II" 
+    blue
+    putStr "&&&&&&&"
+    red
+    putStr "III"
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&&&"
+    stop
+--eleven
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&"
+    red
+    putStr "III ---''IIII'''-."
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&&"
+    stop
+    --twelve
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&"
+    red
+    putStr "IIII"
+    blue
+    putStr "&&&&&&&&&&&&"
+    red
+    putStr "o"
+    blue
+    putStr "&"
+    red
+    putStr "\\"
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&"
+    stop
+    --thirteen
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&"
+    red
+    putStr "IIII"
+    blue
+    putStr "&&&&&&"
+    red
+    putStr ") )"
+    blue
+    putStr "&&&&&&"
+    red
+    putStr "\\"
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&"
+    stop
+    --fourteen
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&"
+    red
+    putStr "III"
+    blue
+    putStr "&"
+    red
+    putStr "--.....III......O"
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&"
+    stop
+    --fifteen
+    blue
+    putStr "&&&&&&&&&&&&&&&&&&&&&&"
+    red
+    putStr "II"
+    blue
+    putStr "&&&&&&&&&"
+    red
+    putStr "II"
+    blue
+    putStrLn "&&&&&&&&&&&&&&&&&&&&&&&&&"
+    stop
+    blueLine
+    blueLine
+    blueLine
+    blueLine
+    blueLine
+    blueLine
+    blueLine
+    blueLine
+    blueLine
+    blueLine
 
 
 loop :: IO ()
@@ -1486,9 +1618,9 @@ loop = do
     screenSet
     frameEleven
     pause
-    --screenSet
-    --frameTwelve
-    --pause
+    screenSet
+    frameTwelve
+    pause
     --screenSet
     --frameThirteen
     --pause
