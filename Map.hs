cfe0bb60346213d3f970fcf0eda365337049e28a
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-28 07:20:08 -0600

    Update Map.hs

--- Map.hs
+++ Map.hs
@@ -21,10 +21,10 @@ list =          ['_','_','_','_','_','_','_','_',
                  '_','_','_','_','_','_','_','_']
                  
               
-printIt :: ([Char] -> IO ()) -> (Char -> IO ())
+printIt :: [Char] -> IO ()
 printIt (x:xs) = 
-        print (take 8 x)
-        print (take 8 xs)
+        [print (take 8 x),
+        print (take 8 xs)]
 
                                                   
 up :: [Char] -> Int -> [Char]
